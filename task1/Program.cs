1bf98aa5e39868e29ed2464a730e2cc313c04f31
«Max» <maks28isaev@gmail.com>
2023-07-13 19:14:48 +0300

    dz

---
+++ task1/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+int ReadInt(string text)
+{
+    System.Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void Numbers(int M, int N)
+{
+    if (M > N) return;
+    System.Console.Write(M + " ");
+    Numbers(M + 1, N);
+
+}
+
+int M = ReadInt("Введите начальное число: ");
+int N = ReadInt("Введите конечное число: ");
+if (M <= N)
+{
+    Numbers(M, N);
+}
+else
+{
+    Numbers(N, M);
+}
+
+System.Console.WriteLine();
+void Printsum(int M, int N, int sum)
+{
+    if (M > N)
+    {
+        Console.WriteLine($"Сумма натуральных элементов в промежутке : {sum}");
+        return;
+    }
+    sum = sum + (M++);
+    Printsum(M, N, sum);
+}
+Printsum(M, N, 0);
+
+
+
+
+
+
+
+
+
+
---
+++ task2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+int ReadInt(string text)
+{
+    System.Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int m = ReadInt("Введите начальное число: ");
+int n = ReadInt("Введите конечное число: ");
+int SumNumbers(int m, int n)
+{
+
+    if (m == 0)
+    { return n + 1; }                                                   // Если m равно нулю
+    else if (n == 0 && m > 0) { return (SumNumbers(m - 1, 1)); }                    // Если m >0 ,n=0
+    else if (m > 0 && n > 0) { return SumNumbers(m - 1, SumNumbers(m, n - 1)); }    // Если m > 0 ,n>0 
+    return SumNumbers(m, n);
+}
+System.Console.WriteLine(SumNumbers(m,n));
